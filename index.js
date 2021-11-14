2c58ea66628098d21184819daa45e7b8291b424f
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-11-14 19:33:56 +0600

    five

--- index.js
+++ index.js
@@ -124,7 +124,15 @@ async function run(){
         const result=await orderCollection.deleteOne(query);
         res.send(result);
       });
-     
+       
+      app.delete('/delproducts/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)};
+        const result=await productCollection.deleteOne(query);
+        res.send(result);
+      });
+       
+
    
 
 
